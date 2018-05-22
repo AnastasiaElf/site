10c8cf6cc446c7e02c6beeecc20372a1d187ebae
NastyaArt <artemchuk.nastya@gmail.com>
2018-05-22 20:24:34 +0300

    add about $ contact pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
+import "./styles.css";
+import me from "./images/me.jpg";
 
 class About extends Component {
   render() {
     return (
       <div>
-        <h1 className="title">My name is Anastasia.</h1>
+        <div className="about_cards">
+          <div className="about_card">
+            <img src={me} className="me_image" alt="image" />
+            <div className="text">
+              My name is Anastasia. I'm a cheerful pessimist and just a good
+              person =) <br /> Don't even try to understand me, otherwise you'll
+              break your head =D
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
+import "./styles.css";
+import gmail from "./images/gmail.png";
+import github from "./images/github.svg";
 
 class Contact extends Component {
   render() {
     return (
       <div>
-        <h1 className="title">My mail: anastasia.elf.17@gmail.com</h1>
+        <div className="contacts_card">
+          <div className="contact">
+            <img src={gmail} className="image_contact" alt="image" />
+            <div> Email: anastasia.elf.17@gmail.com </div>
+          </div>
+          <div className="contact">
+            <img src={github} className="image_contact" alt="image" />
+            <div>Github: AnastasiaElf</div>
+          </div>
+        </div>
       </div>
     );
   }
