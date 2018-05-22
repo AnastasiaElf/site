d7ad6a7254ffdd48fe5015f5cbe19a1fa52580ae
NastyaArt <artemchuk.nastya@gmail.com>
2018-05-22 15:37:52 +0300

    change icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./images/elf.png";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -8,11 +8,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="App-title">Welcome to my world.</h1>
       </div>
     );
   }
